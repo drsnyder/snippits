10ddd0a54d8a96665a330c15800cf94746ef7e65
Damon Snyder <drsnyder@gmail.com>
2009-06-30 15:42:23 -0700

    Updated some of the sorting and prime tests.

--- py/algo/sorting.py
+++ py/algo/sorting.py
@@ -6,9 +6,60 @@ def build_list(n):
 def bubble(l):
     swapped = True
     n = 0
+    count = 0
     while swapped:
-        for i in len(l) - 1:
+        swapped = False
+        for i in range(len(l) - 1):
+            count += 1
             if l[i] > l[i + 1]:
+                temp = l[i]
+                l[i] = l[i + 1]
+                l[i + 1] = temp
+                swapped = True
+
+    return count
 
 
-print build_list(10)
+def concat(less, pivot, greater):
+    final = []
+    for e in less:
+        final.append(e)
+    final.append(pivot)
+    for e in greater:
+        final.append(e)
+
+    return final
+        
+def quick(l):
+    print "# quick #"
+    if len(l) <= 1:
+        return l
+
+    pivot = l[-1]
+    l.pop()
+    less = []
+    greater = []
+    for i in range(len(l)):
+        print "looking at pivot %d and %d" % (pivot, l[i])
+        if l[i] > pivot: 
+            greater.append(l[i])
+        else:
+            less.append(l[i])
+
+    return concat(quick(less), pivot, quick(greater))
+
+
+
+l = build_list(100)
+iter = bubble(l)
+print "Required %d iterations from %d items" % (iter, len(l))
+
+l.reverse()
+iter = bubble(l)
+print "Required %d iterations from %d items" % (iter, len(l))
+
+print "######################"
+
+x = build_list(100)
+quick(x)
+
--- py/pe/lgstpfactory.py
+++ py/pe/lgstpfactory.py
@@ -1,20 +1,26 @@
 from math import *
 def isprime(n):
-    start = 2
+    if n == 1 or n == 2:
+        return True
+
+    if (n % 2) == 0:
+        return False
+
+    start = 3
     stop = round(sqrt(n))
-    while start < stop:
-        if start != 2 and (start % 2) == 0:
+    while start <= stop:
+        #print "isprime: %d %% %d = %d --> %d" % (n, start, n % start, stop)
+        if (start % 2) == 0:
             # skip factors of 2
             start += 1
             continue
 
         if (n % start) == 0: 
-            break
+            return False
         start += 1
-    else:
-        return True
 
-    return False
+    return True
+
 
 def findlargestpfactordown(n):
     start = round(sqrt(n))
@@ -32,13 +38,35 @@ def findlargestpfactordown(n):
         start -= 1
 
     return start
+
+def find_nth_prime(n):
+    pcount = 0
+    i = 1
+    while pcount < n:
+        i += 1
+        if i != 2 and (i % 2) == 0:
+            continue
+
+        if isprime(i):
+            #print "%d %d %d" % (pcount, i, n)
+            pcount += 1
+
+
+    return i
+
+
+
         
 
 
-print isprime(35)
-print isprime(17)
-print isprime(11)
-print isprime(10)
+print "35 ",isprime(35)
+print "17 ",isprime(17)
+print "11 ",isprime(11)
+print "10 ",isprime(10)
+print "7621 ",isprime(7621)
+print "9 ",isprime(9)
 
-print findlargestpfactordown(13195)
-print findlargestpfactordown(600851475143)
+#print findlargestpfactordown(13195)
+#print findlargestpfactordown(600851475143)
+
+print find_nth_prime(100000)
