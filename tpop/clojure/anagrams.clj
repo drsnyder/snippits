f48f6ee81f9a2c8aa270fb3a008098d876016a0d
Damon Snyder <drsnyder@damon-snyders-imac.local>
2010-08-14 22:04:34 -0700

    updated anagrams

--- tpop/clojure/anagrams.clj
+++ tpop/clojure/anagrams.clj
@@ -14,4 +14,25 @@
 (defn str-to-lower[str]
   (.toLowerCase str))
 
+
+(defn firstn[seq n]
+  (drop-last (- (count seq) n) seq))
+
+
+(defn anagram-add[anagrams akey word]
+  (if (empty? (get anagrams akey))
+    (assoc anagrams akey (hash-map :count 1, :words (list word)))
+    (update-in (update-in anagams akey [:count] inc) [:words] conj word)))
+
+(defn build-anagrams[seq]
+  (loop [left seq word (first left) akey (str-sort (str-to-lower word)) anagrams {}]
+    (if (empty? left)
+      anagrams
+      (do 
+        (anagram-add anagrams akey word)
+        (recur (rest left))))))
+
+
+(def words (f-to-seq "/usr/share/dict/web2"))
+(def anagrams #{})
 (count (f-to-seq "/usr/share/dict/web2"))
