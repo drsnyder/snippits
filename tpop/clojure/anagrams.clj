4519fa490c070eef467c4c812223f9f91a887ab0
Damon Snyder <drsnyder@gmail.com,damon@huddler-inc.com>
2010-08-16 20:49:50 -0700

    printing top 10 sorted anagrams

--- tpop/clojure/anagrams.clj
+++ tpop/clojure/anagrams.clj
@@ -1,38 +1,52 @@
 
 ;(use 'clojure.test')
 
-(println "hello")
-
 (defn f-to-seq[file]
   (with-open [rdr (java.io.BufferedReader. 
                     (java.io.FileReader. file))]
     (doall (line-seq rdr))))
 
 (defn str-sort[str]
-  (String. (into-array (. Character TYPE) (sort str))))
+  (if (nil? str)
+    str
+  (String. (into-array (. Character TYPE) (sort str)))))
 
 (defn str-to-lower[str]
-  (.toLowerCase str))
-
-
-(defn firstn[seq n]
-  (drop-last (- (count seq) n) seq))
+  (if (nil? str)
+    str
+    (.toLowerCase str)))
 
 
 (defn anagram-add[anagrams akey word]
   (if (empty? (get anagrams akey))
     (assoc anagrams akey (hash-map :count 1, :words (list word)))
-    (update-in (update-in anagams akey [:count] inc) [:words] conj word)))
+    (update-in (update-in anagrams [akey :count] inc) [akey :words] conj word)))
+
+(defn word[seq] (first seq))
 
 (defn build-anagrams[seq]
-  (loop [left seq word (first left) akey (str-sort (str-to-lower word)) anagrams {}]
-    (if (empty? left)
+  (loop [seq seq 
+         akey (str-sort (str-to-lower (word seq))) 
+         anagrams {}]
+    (if (empty? seq)
       anagrams
-      (do 
-        (anagram-add anagrams akey word)
-        (recur (rest left))))))
+      (recur (rest seq) 
+             (str-sort (str-to-lower (word (rest seq)))) 
+             (anagram-add anagrams akey (word seq))))))
 
 
-(def words (f-to-seq "/usr/share/dict/web2"))
-(def anagrams #{})
-(count (f-to-seq "/usr/share/dict/web2"))
+(defn print-anagram[v]
+  (println (str (:count (first (rest v))) " " (first v) ": " (:words (first (rest v))))))
+
+(defn print-anagrams[ana]
+  (doseq [v ana] 
+    (print-anagram v)))
+
+(defn anagram-key[elem] (:count (first (rest elem)))) 
+
+(def *words* (f-to-seq "/usr/share/dict/web2"))
+(def *anagrams* (sort-by anagram-key (build-anagrams *words*)))
+(print-anagrams (take 10 (reverse *anagrams*)))
+
+;(build-anagrams (take 5 words))
+;(count (f-to-seq "/usr/share/dict/web2"))
