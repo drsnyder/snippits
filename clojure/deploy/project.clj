4d5ca48c58b1a0d08bdaef46dd95f93488382381
Damon Snyder <drsnyder@gmail.com>
2011-03-31 07:25:26 -0700

    Adding a deploy project for testing.

---
+++ clojure/deploy/project.clj
@@ -0,0 +1,5 @@
+(defproject deploy "0.0.1-SNAPSHOT"
+  :description "TODO: add summary of your project"
+  :dependencies [[clojure "1.2.0"]
+                 [clojure-contrib "1.2.0"]
+                 [clj-ssh "0.2.0"]])
